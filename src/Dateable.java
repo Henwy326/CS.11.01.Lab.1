03476ecdf502c725038cf8d3afdb345e01d50f3b
henwy326 <henrysr2028@student.cis.edu.hk>
2025-09-10 10:00:37 +0800

    Initial Commit

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
-public class Dateable {
-
-    public static void main(String[] args) {
+import java.util.Scanner; 
+public class Dateable { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
         
-    }
-}
+        int age = scanner.nextInt(); scanner.close();
+        int youngestAge = (7+(age/ 2)); 
+
+        System.out.println("You can date someone who is at least " + youngestAge + " years of age."); 
+} }
